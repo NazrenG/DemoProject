b63a45408789855a347457f7256b89bf8698d164
Sevgi <sevgi.elesgerova@gmail.com>
2024-10-18 00:44:02 +0400

    nothingChanged

--- DemoProject/DTOs/UserForRegiter.cs
+++ DemoProject/DTOs/UserForRegiter.cs
@@ -5,7 +5,7 @@ namespace DemoProject.DTOs
     {
         public string Username { get; set; }
         public string Email { get; set; }
-        public string AgeGroup { get; set; }
+        //public string AgeGroup { get; set; }
         public string Password { get; set; }
     }
 }
